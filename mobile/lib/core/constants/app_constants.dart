4f9294502f30c009c9ac802fbe125759fdc7be1c
BlckIt <blckit@BlckIt.local>
2026-06-06 00:56:32 +0200

    chore(mobile): bump to v1.5.2+10 (background sync + create-4xx parity)

--- mobile/lib/core/constants/app_constants.dart
+++ mobile/lib/core/constants/app_constants.dart
@@ -1,5 +1,5 @@
 /// Default gateway when the user hasn't set one. The self-hosted box on the LAN.
 const String kDefaultBaseUrl = 'http://127.0.0.1:18789';
-const String kAppVersion = '1.5.1';
-const int kAppBuild = 9;
+const String kAppVersion = '1.5.2';
+const int kAppBuild = 10;
 const String kSecureBaseUrlKey = 'lazyclaw_base_url';

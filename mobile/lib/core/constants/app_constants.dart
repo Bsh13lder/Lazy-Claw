e64f658b4afc962391f783588770d2128938ff3a
BlckIt <blckit@BlckIt.local>
2026-06-05 17:13:40 +0200

    feat(mobile): server config + url normalization

---
+++ mobile/lib/core/constants/app_constants.dart
@@ -0,0 +1,5 @@
+/// Default gateway when the user hasn't set one. The self-hosted box on the LAN.
+const String kDefaultBaseUrl = 'http://127.0.0.1:18789';
+const String kAppVersion = '0.1.0';
+const int kAppBuild = 1;
+const String kSecureBaseUrlKey = 'lazyclaw_base_url';
